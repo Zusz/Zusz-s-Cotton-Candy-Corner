cc0768885ecb721b03007e49d29b617d6d4362d6
Zusz <zusz2011@gmail.com>
2025-12-28 15:54:48 +0100

    -Added model for CottonCandyMachine

--- src/main/java/net/zusz/zcottoncandymod/block/custom/CottonCandyMachineBlock.java
+++ src/main/java/net/zusz/zcottoncandymod/block/custom/CottonCandyMachineBlock.java
@@ -1,30 +1,84 @@
 package net.zusz.zcottoncandymod.block.custom;
 
 import com.mojang.serialization.MapCodec;
-import net.minecraft.block.BlockEntityProvider;
-import net.minecraft.block.BlockRenderType;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.BlockWithEntity;
+import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.NamedScreenHandlerFactory;
-import net.minecraft.util.Hand;
-import net.minecraft.util.ItemActionResult;
-import net.minecraft.util.ItemScatterer;
+import net.minecraft.util.*;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import net.zusz.zcottoncandymod.block.entity.CottonCandyMachineBlockEntity;
 import org.jetbrains.annotations.Nullable;
 import net.zusz.zcottoncandymod.block.entity.ModBlockEntities;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.Direction;
 
 public class CottonCandyMachineBlock extends BlockWithEntity implements BlockEntityProvider {
     public static MapCodec<CottonCandyMachineBlock> CODEC = CottonCandyMachineBlock.createCodec(CottonCandyMachineBlock::new);
+
+    public static final VoxelShape SHAPE = VoxelShapes.union(
+            Block.createCuboidShape(5,0,5,11,6,11),
+            Block.createCuboidShape(3, 6, 3, 13, 10, 13)
+
+    );
+    public static final DirectionProperty FACING = Properties.HORIZONTAL_FACING;
     public CottonCandyMachineBlock(Settings settings) {
         super(settings);
+        this.setDefaultState(this.getStateManager().getDefaultState()
+                .with(FACING, Direction.NORTH));
+    }
+
+    @Override
+    protected VoxelShape getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return SHAPE;
+    }
+
+    @Override
+    protected VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return SHAPE;
+    }
+
+    @Override
+    protected VoxelShape getCullingShape(BlockState state, BlockView world, BlockPos pos) {
+        return SHAPE;
+    }
+
+    @Override
+    protected boolean isTransparent(BlockState state, BlockView world, BlockPos pos) {
+        return true;
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+        super.appendProperties(builder);
+    }
+
+    @Nullable
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext context) {
+        return this.getDefaultState().with(FACING, context.getHorizontalPlayerFacing().getOpposite());
+    }
+
+    @Override
+    protected BlockState rotate(BlockState state, BlockRotation rotation) {
+        return state.with(FACING, rotation.rotate(state.get(FACING)));
+    }
+
+    @Override
+    protected BlockState mirror(BlockState state, BlockMirror mirror) {
+        return rotate(state, mirror.getRotation(state.get(FACING)));
     }
 
     @Override
